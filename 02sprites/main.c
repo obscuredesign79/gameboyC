54e7d70d721bff7cd136b347a90adcb8d265c40f
Bryan Lorenzo <obscuredesign79@gmail.com>
2019-11-28 13:14:58 +0800

    2019-11-28

--- 02sprites/main.c
+++ 02sprites/main.c
@@ -17,12 +17,32 @@ void main()
 	UINT8 currentSpriteBlinker = 0;
 	UINT8 currentSpritePacMan = 2;
 
-    set_sprite_data(0, 2, blinker);//(starts of tile, how manytiles, name of variable sprite(not the fileName.c))
-    set_sprite_tile(0, 0);//(tile number to load, sprite index if more than one tile can change)
-    move_sprite(0, 8, 16);//(sprite ID, x, y)
+	/*	Note: 
+		set_sprite_data() loads the sprite to the vram.
+		The first variable is the tile number in the vram.
+		If more than one sprite is loaded, enter the second
+		sprite to the next available tile in the vram taking
+		into account the tile number used on the first sprite
+		with all the extra tiles for animation.
+		
+		set_sprite_tile() sets the sprite id on the first flag
+		to be used in move_sprite() to differentiate the different
+		sprites from one another. The second flag is the tile
+		number in the vram.
+
+		move_sprite() displays the sprite on the gameboy screen.
+		the first flag is the sprite id indicated in the set_sprite_tile().
+		The second and third flag is the x and y location on the gameboy
+		screen. Note that the location (8, 16) is the top left most
+		corner of the screen.
+	*/
+
+    set_sprite_data(0, 2, blinker);
+    set_sprite_tile(0, 0);
+    move_sprite(0, 8, 16);
 
 	set_sprite_data(2, 2, pacMan);
-	set_sprite_tile(2, 1);
+	set_sprite_tile(1, 2);
 	move_sprite(1, 16, 16);
 
 
@@ -41,7 +61,7 @@ void main()
 			currentSpritePacMan = 2;
 		}
 		set_sprite_tile(0, currentSpriteBlinker);
-		set_sprite_tile(2, currentSpritePacMan);
+		set_sprite_tile(1, currentSpritePacMan);
 
 		performantDelay(5);
 	}
